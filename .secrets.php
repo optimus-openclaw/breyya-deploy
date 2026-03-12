d70ebc21dcf163ca1bf2d76b34826a6009f57203
Optimus <optimus@Optimuss-Mini.mynetworksettings.com>
2026-03-11 23:47:31 -0700

    Deploy: add .secrets.php for OpenAI chat, rebuild with Test as Fan toggle

---
+++ .secrets.php
@@ -0,0 +1,3 @@
+<?php
+define('OPENAI_API_KEY', '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');
+define('CHAT_CRON_SECRET', 'breyya-chat-cron-2026');
